97c714833202fd9bed8aaf632bd194bab79f3358
RAPHAEL LEITE CAMPOS <prof.raphaellc@gmail.com>
2016-09-28 09:41:19 -0300

    Arquivos FPS

---
+++ Assets/Atirar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atirar : MonoBehaviour {
+	public GameObject bala;
+	public Transform posicaoBala;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.T))
+			Instantiate (bala, posicaoBala.position, posicaoBala.rotation );
+	}
+}
---
+++ Assets/Tiro.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiro : MonoBehaviour {
+
+	public Rigidbody RBTiro;	
+	public float v;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, v * Time.deltaTime);
+		Destroy (gameObject,2);
+	}
+}
